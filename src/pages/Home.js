6d5c6b55a4e4fc783df085e5c4267d5b4a680125
Paskuvan <majopaskuvan@gmail.com>
2022-04-21 16:29:52 -0400

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import bg from "../images/frontpagebg.png";
+import logo from "../images/airbnb.png";
+
+const Home = () => {
+
+  return (
+    <>
+      <div className="container" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="containerGradient"></div>
+      </div>
+      <div  className="topBanner">
+        <div>
+          <img className="logo" src={logo} alt="logo"></img>
+        </div>
+        <div className="tabs">
+          <div className="selected">Places To Stay</div>
+          <div>Experiences</div>
+          <div>Online Experiences</div>
+        </div>
+        <div className="lrContainers">
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
